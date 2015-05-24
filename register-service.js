96f150bcb09a95f36a96efc0b95ad48791d55222
Seth Lakowske <lakowske@gmail.com>
2015-05-24 00:38:52 -0500

    listen for SIGINT ctrl-c

--- register-service.js
+++ register-service.js
@@ -9,29 +9,28 @@ var url      = require('url');
 var register = require('./').register;
 var remove   = require('./').remove;
 
-console.log(process.argv);
-
 var connectOptions = {}
-if (process.argv.length > 6) {
+if (process.argv.length >= 8) {
     var serviceDirectory = process.argv[2];
-    console.log(serviceDirectory);
     connectOptions = url.parse(serviceDirectory);
     var serviceName = process.argv[3];
     var hostName = process.argv[4];
     var port     = process.argv[5];
-    console.log(serviceName);
     var usedArgs = process.argv.splice(0, 6);
 
-    console.log(usedArgs);
-    console.log(process.argv);
+    function shutdown() {
+        remove(connectOptions, serviceName, function() {
+            console.log('process died and removed service');
+            process.exit();
+        })
+    }
+
     register(connectOptions, serviceName, hostName, port, {}, function() {
         var service = spawn('node', process.argv);
-        service.on('close', function() {
-            remove(connectOptions, serviceName, function() {
-                console.log('process died and removed service');
-            })
-        })
+        service.on('close', shutdown);
     });
+
+    process.on('SIGINT', shutdown);
 
 } else {
     console.log('Usage: register-service <service directory URL> <service name> <script path> [[[port] args ] ... ]');
